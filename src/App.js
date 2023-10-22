7ac6d190ce5566dda4f7c2d6bb899f9611abef67
avshir <av.shirinskaya@gmail.com>
2023-10-22 23:48:08 +0200

    feat: add Title component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Likes from './Likes';
+import Title from './Title';
+
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
         <div className="card">
           <div className="card-image">
             <img src="./sea.jpg" alt="surfing" />
+            <Title />
             <Likes />
           </div>
         </div>
